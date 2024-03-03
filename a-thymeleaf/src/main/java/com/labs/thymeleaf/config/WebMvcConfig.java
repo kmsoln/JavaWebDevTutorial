28f813cc06fc7ad6153ed08ba736ade3045f4221
kmsoln <kmsoln@vk.com>
2024-03-03 19:42:29 +0300

    [thymeleaf] moved first view

--- a-thymeleaf/src/main/java/com/labs/thymeleaf/config/WebMvcConfig.java
+++ a-thymeleaf/src/main/java/com/labs/thymeleaf/config/WebMvcConfig.java
@@ -42,6 +42,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
         // First View
-        registry.addViewController("/first").setViewName("task-one/first");
+        registry.addViewController("/first").setViewName("tasks/first");
     }
 }
