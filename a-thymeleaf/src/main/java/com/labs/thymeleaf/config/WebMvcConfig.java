cb8f82250f2af916b4f7d37cc0bbd0422d769409
kmsoln <kmsoln@vk.com>
2024-03-03 19:38:26 +0300

    [thymeleaf] Modified first view

--- a-thymeleaf/src/main/java/com/labs/thymeleaf/config/WebMvcConfig.java
+++ a-thymeleaf/src/main/java/com/labs/thymeleaf/config/WebMvcConfig.java
@@ -2,13 +2,17 @@ package com.labs.thymeleaf.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
 
     @Bean
     public ClassLoaderTemplateResolver templateResolver() {
@@ -20,9 +24,24 @@ public class WebMvcConfig implements WebMvcConfigurer {
         return templateResolver;
     }
 
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+
+        return templateEngine;
+    }
+
+    @Bean
+    public ViewResolver thymeleafViewResolver() {
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setTemplateEngine(templateEngine());
+
+        return resolver;
+    }
+
     public void addViewControllers(ViewControllerRegistry registry) {
-        // Home
-        registry.addViewController("/home/index").setViewName("/home/index");
-        registry.addRedirectViewController("/", "/home/index");
+        // First View
+        registry.addViewController("/first").setViewName("task-one/first");
     }
 }
